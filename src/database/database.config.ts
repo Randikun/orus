100899b635ff4628bf4fbc6f60d1e3268115c89e
Franco1312 <francokail2@gmail.com>
2022-12-19 15:59:57 -0300

    Change ORM setup implementation and create UserEntity

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,50 +1,34 @@
-import { config, parse } from 'dotenv'
-import { join } from 'path';
+import { config, parse } from 'dotenv';
 
 export class DatabaseConfig {
-    private readonly envConfig: Record<string, string>;
+  private readonly envConfig: Record<string, string>;
 
-    constructor() {
-        config();
-        this.envConfig = { ...process.env, ...parse };
-    }
+  constructor() {
+    config();
+    this.envConfig = { ...process.env, ...parse };
+  }
 
-    public get(key: string) {
-        return this.envConfig[key]
-    }
+  public get(key: string) {
+    return this.envConfig[key];
+  }
 
-    public getDatabaseHost() {
-        return this.get('DB_HOST')
-    }
+  public getDatabaseHost() {
+    return this.get('DB_HOST');
+  }
 
-    public getDatabasePort() {
-        return this.get('DB_PORT')
-    }
+  public getDatabasePort() {
+    return this.get('DB_PORT');
+  }
 
-    public getDatabaseUsername() {
-        return this.get('DB_USERNAME')
-    }
+  public getDatabaseUsername() {
+    return this.get('DB_USERNAME');
+  }
 
-    public getDatabasePassword() {
-        return this.get('DB_PASSWORD')
-    }
+  public getDatabasePassword() {
+    return this.get('DB_PASSWORD');
+  }
 
-    public getDatabase() {
-        return this.get('DB_DATABASE')
-    }
-
-
-    public getMysqlConnectionOptions() {
-        return {
-            type: 'mysql',
-            host: this.getDatabaseHost(),
-            port: Number(this.getDatabasePort),
-            username: this.getDatabaseUsername(),
-            password: this.getDatabasePassword(),
-            database: this.getDatabase(),
-            synchronize: false,
-            migrationsRun: false,
-            entities: [join(__dirname, '/../**/**.entity{.ts,.js}')],
-        }
-    }
+  public getDatabase() {
+    return this.get('DB_DATABASE');
+  }
 }
