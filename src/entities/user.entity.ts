5077f3a9ec7f3b884482810d7599ac4898202b17
Franco1312 <francokail2@gmail.com>
2023-01-23 19:58:12 -0300

    Users migration created

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -5,18 +5,50 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: false, name: 'first_name' })
+  @Column({
+    nullable: false,
+    name: 'first_name',
+    type: 'varchar',
+    length: '36'
+  })
+  uuid: string;
+
+  @Column({
+    nullable: false,
+    name: 'first_name',
+    type: 'varchar',
+    length: '36'
+  })
   firstName: string;
 
-  @Column({ nullable: false, name: 'last_name' })
+  @Column({
+    nullable: false,
+    name: 'last_name',
+    type: 'varchar',
+    length: '36'
+  })
   lastName: string;
 
-  @Column({ nullable: false, name: 'email' })
+  @Column({
+    nullable: false,
+    name: 'email',
+    type: 'varchar',
+    length: '36'
+  })
   email: string
 
-  @Column({ nullable: false, name: 'password' })
+  @Column({
+    nullable: false,
+    name: 'password',
+    type: 'varchar',
+    length: '100',
+  })
   password: string;
 
-  @Column({ default: true, name: 'is_active' })
+  @Column({
+    default: true,
+    name: 'is_active',
+    type: 'boolean'
+  })
   isActive: boolean;
 }
